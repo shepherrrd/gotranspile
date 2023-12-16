7aeedc1fffeb6e0d7f5070345bba87f55a2ef9ac
shepherrrd <shefkid2@gmail.com>
2023-12-16 21:12:03 +0100

    Added Command handling and file reading

--- src/main.go
+++ src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-   fmt.Println("Hello, Transpile")
+   
+   if len(os.Args) < 2 {
+      log.Fatal("Usage: go run main.go [path_to_json_file]")
+  }
+  filePath := os.Args[1]
+  data, err := os.ReadFile(filePath)
+  if err != nil {
+      log.Fatalf("Error reading file: %s", err)
+  }
+  jsonStr := string(data)
+  fmt.Println(jsonStr)
 }

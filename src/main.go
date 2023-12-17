3d3a73bd1b016bc4f51e2e465c5d066c88ccd571
shepherrrd <shefkid2@gmail.com>
2023-12-17 19:10:42 +0100

    Fixed encoding/json trying failing to cast int and casting it as float64

--- src/main.go
+++ src/main.go
@@ -15,6 +15,7 @@ func main() {
   }
    if len(os.Args) < 2 {
       log.Fatal("Usage: go run main.go [path_to_json_file]")
+      return
   }
   var jsonObj map[string]interface{}
   filePath := os.Args[1]
@@ -25,18 +26,23 @@ func main() {
    protoStorageTypes := make(map[string]Item)
   jsonStr := string(data)
   er2 := json.Unmarshal([]byte(jsonStr), &jsonObj)
+  fmt.Print(jsonStr)
     if er2 != nil {
         log.Fatal(err)
     }
     for key, value := range jsonObj {
       j := getValueType(value)
+      if num, ok := value.(float64); ok {
+        if num == float64(int64(num)) {
+            j = reflect.TypeOf(int(0))
+        }
+    }
+    print(value)
       protoStorageTypes[key] = Item{Value: value, Type: j}
   }
-
-  for key,value := range protoStorageTypes{
-   fmt.Printf("The Name of the proto data is %s and the value is %s and type is %s \n",key,value.Value,value.Type)
+  for d , j := range protoStorageTypes {
+    fmt.Println(d ,j.Value, j.Type)
   }
-  print(reflect.TypeOf(30))
 }
 func getValueType(variableToCheck interface{}) reflect.Type{
  return reflect.TypeOf(variableToCheck)
